54442a01707bb2f3c14d2ead8a4dedb31dc5e039
John L Mitchell <johnlmitchell@gmail.com>
2010-04-24 21:18:33 -0700

    finds where values are, for more precise splitting

--- parse.py
+++ parse.py
@@ -4,18 +4,29 @@ ERR='''get_slice(column_parent=ColumnParent(column_family='words', super_column=
 ERR2='''get_slice(arg=call(c1='c1', c2=None), key='ding')'''
 
 import ast, re
+from itertools import ifilter
 
 code=ERR
 
 class CallCode(list):
     def __init__(self, code):
         super(CallCode,self).__init__()
+        self.name = None
         self.parse(code)
+        self.valuepos = list(self.findvalues(self.mod))
+
+    def findvalues(self, topnode):
+        def expr_order(topnode):
+            for node in ifilter(
+                lambda node: hasattr(node, 'lineno'), 
+                ast.walk(topnode)):
+                yield (node.lineno, node.col_offset)
+        return sorted(expr_order(topnode))
 
     def parse(self, code):
         self.code = code
-        mod = ast.parse(code)
-        expr = ast.iter_child_nodes(mod).next()
+        self.mod = ast.parse(code)
+        expr = ast.iter_child_nodes(self.mod).next()
         call = ast.iter_child_nodes(expr).next()
         for arg in ast.iter_child_nodes(call):
             if type(arg) is ast.Name:
@@ -26,7 +37,7 @@ class CallCode(list):
                     v_lineno=arg.value.lineno,
                     v_offset=arg.value.col_offset,
                     ) )
-        maxpos=-1
+        maxpos = -1
         for arg in reversed(self):
             arg['v_code'] = self.code[arg['v_offset']:maxpos]
             maxpos = code.rfind(',',None,arg['v_offset'])
