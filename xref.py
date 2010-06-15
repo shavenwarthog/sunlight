6a50c64752b24269d521340c374b1360c6fcbbbd
John Mitchell <john@geodelic.com>
2010-06-14 17:53:34 -0700

    xref works. Now starting special "test X calls function Y" handling

--- xref.py
+++ xref.py
@@ -4,30 +4,32 @@
 xref.py -- postprocess Etags database into call graph
 '''
 
-import os, re
+import os, re, sys
 from itertools import imap
 from nose.tools import eq_ as eq
 from operator import itemgetter
 
-tagsdata = os.popen('ctags -f - zoot.py')
+verbose = 2
 
-symbols = {}
-for name,path,_,rest in imap(lambda line: line.split('\t',3), tagsdata):
-    symbols[name] = rest[0]     # symbol type
-
-# defs = list( imap(itemgetter(1), def_pat.findall(tagsdata)) )
-defs = symbols.keys()
-print 'defs:',defs
+if 0:
+    tagsdata = os.popen('ctags -f - zoot.py')
+    symbols = {}
+    for name,path,_,rest in imap(lambda line: line.split('\t',3), tagsdata):
+        symbols[name] = rest[0]     # symbol type
+    # defs = list( imap(itemgetter(1), def_pat.findall(tagsdata)) )
+    defs = symbols.keys()
 
 defpat = re.compile(
     '(def|class)\s+(\w+)',
     )
+# callpat = re.compile(
+#     '(%s)\s*\(' % '|'.join(defs)
+#     )
 callpat = re.compile(
-    '(%s)\s*\(' % '|'.join(defs)
+    # '(%s)\s*(\(.*\)?)',
+    '([A-Za-z_][A-Za-z0-9_]*)\s*\(',
     )
 
-# os.system('egrep -n . zoot.py')
-# print
 
 # etags format:
 #	{tag_definition_text}<\x7f>{tagname}<\x01>{line_number},{byte_offset}
@@ -47,9 +49,22 @@ def test_enum_pos():
         [(0, 'tasty', 0, 6), (1, 'beer', 6, 11)],
         )
 
+PRIMLIST = set('set int len range reduce type __init__'.split())
+
+class TagsFile(list):
+    def append(self, path, tagdef, tagname, lineno, offset):
+        self += (path, tagdef, tagname, lineno, offset)
+    def writeto(self, outpath):
+        outf = open(outpath, 'w')
+
 def main(paths):
     tagsf = open('calls.tags', 'w')
     for path in paths:
+        if not path.endswith('.py'):
+            continue
+        if '/lib/python' in path:
+            continue
+        print '%s:' % path
         source = open(path, 'r').read()
         print >> tagsf, '\f'
         print >> tagsf, '%s,%s' % (path, len(source))      # X?
@@ -57,20 +72,29 @@ def main(paths):
 
         in_def = None
         for num,line,begpos,_ in enum_pos(source.split('\n')):
-            print '%2d %3d %s' % (num+1, begpos, line)
+            if verbose:
+                print '%2d %3d %s' % (num+1, begpos, line)
             m = defpat.search(line)
             if m:
                 in_def = m.group(2), num+1, begpos
-                print 'def', in_def
-            else:
-                for m in callpat.finditer(line):
-                    print '\t', m.group(1), '=>', in_def[0]
-                    print >> tagsf, symref(
-                        tagdef=line,
-                        tagname=m.group(1),
-                        lineno=num+1, 
-                        offset=begpos,
-                        )
+                # print 'def', in_def
+                continue
+            for m in callpat.finditer(line):
+                calls = m.group(1)
+                if calls in PRIMLIST:
+                    continue
+                if not in_def:
+                    continue    # XXX?
+                if verbose:
+                    print '- %s: calls %s%s' % (in_def[0], calls, '()')
+                print >> tagsf, symref(
+                    tagdef=line,
+                    tagname=m.group(1),
+                    lineno=num+1, 
+                    offset=begpos,
+                    )
     tagsf.close()
 
-main(['zoot.py'])
+if __name__=='__main__':
+    main(sys.argv[1:])
+    # main([os.path.abspath('zoot.py')])
