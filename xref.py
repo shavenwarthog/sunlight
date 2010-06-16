d6deb307d9225034f95a3012dcb345523a2bf6f2
John Mitchell <john@geodelic.com>
2010-06-16 14:48:37 -0700

    works

--- xref.py
+++ xref.py
@@ -4,31 +4,10 @@
 xref.py -- postprocess Etags database into call graph
 '''
 
-import os, re, sys
+import optparse, os, re, sys
 from itertools import imap
 from nose.tools import eq_ as eq
 from operator import itemgetter
-
-verbose = 0
-
-if 0:
-    tagsdata = os.popen('ctags -f - zoot.py')
-    symbols = {}
-    for name,path,_,rest in imap(lambda line: line.split('\t',3), tagsdata):
-        symbols[name] = rest[0]     # symbol type
-    # defs = list( imap(itemgetter(1), def_pat.findall(tagsdata)) )
-    defs = symbols.keys()
-
-defpat = re.compile(
-    '(def|class)\s+(\w+)',
-    )
-# callpat = re.compile(
-#     '(%s)\s*\(' % '|'.join(defs)
-#     )
-callpat = re.compile(
-    # '(%s)\s*(\(.*\)?)',
-    '([A-Za-z_][A-Za-z0-9_]*)\s*\(',
-    )
 
 
 # etags format:
@@ -49,7 +28,7 @@ def test_enum_pos():
         [(0, 'tasty', 0, 6), (1, 'beer', 6, 11)],
         )
 
-PRIMLIST = set('format int len range reduce set type __init__'.split())
+IGNORELIST = set('format int len range reduce set super type __init__'.split())
 
 class TagsFile(list):
     def append(self, path, tagdef, tagname, lineno, offset):
@@ -72,32 +51,55 @@ class TagsFile(list):
                     )
         outf.close()
 
-def main(paths):
+
+def main(argv):
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                      help="write report to FILE", metavar="FILE")
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      )
+
+    (options, paths) = parser.parse_args()
+
+    defpat = re.compile(
+        '(def|class)\s+(\w+)',
+        )
+    callpat = re.compile(
+        # '(%s)\s*(\(.*\)?)',
+        '([A-Za-z_][A-Za-z0-9_]*)\s*\(',
+        )
+
     tagsf = TagsFile()
+    testf = TagsFile()
+    dirs = set()
     for path in paths:
         if not path.endswith('.py'):
             continue
         if '/lib/python' in path:
             continue
-        print '%s:' % path
+        pathdir = os.path.dirname(path)
+        if options.verbose:
+            if pathdir not in dirs:
+                print pathdir
+                dirs.add(pathdir)
         source = open(path, 'r').read()
 
         in_def = None
         for num,line,begpos,_ in enum_pos(source.split('\n')):
-            if verbose:
+            if options.verbose:
                 print '%2d %3d %s' % (num+1, begpos, line)
             m = defpat.search(line)
             if m:
                 in_def = m.group(2), num+1, begpos
-                # print 'def', in_def
                 continue
             for m in callpat.finditer(line):
                 calls = m.group(1)
-                if calls in PRIMLIST:
+                if calls in IGNORELIST:
                     continue
                 if not in_def:
                     continue    # XXX?
-                if verbose:
+                if options.verbose:
                     print '- %s: calls %s%s' % (in_def[0], calls, '()')
                 tagsf.append(
                     path=path,
@@ -106,14 +108,7 @@ def main(paths):
                     lineno=num+1, 
                     offset=begpos,
                     )
-#                 print >> tagsf, symref(
-#                     tagdef=line,
-#                     tagname=m.group(1),
-#                     lineno=num+1, 
-#                     offset=begpos,
-#                     )
-    tagsf.writeto('calls.tags')
+    tagsf.writeto( options.filename )
 
 if __name__=='__main__':
-    main(sys.argv[1:])
-    # main([os.path.abspath('zoot.py')])
+    main(sys.argv)
