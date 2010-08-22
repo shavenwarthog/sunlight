fc0068872b8fd37adf0028f40633d75cfa7fb744
John L Mitchell <johnlmitchell@gmail.com>
2010-08-21 18:39:25 -0700

    tracing external source, works

--- snoop.py
+++ snoop.py
@@ -2,7 +2,7 @@
 snoop.py -- log local vars at end of unit test
 '''
 
-import copy, inspect, keyword, logging, sys, tokenize
+import copy, inspect, keyword, logging, sys, tokenize, trace
 from nose import inspector
 from nose.inspector import Expander as nose_Expander
 
@@ -10,77 +10,6 @@ from nose.tools import eq_
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::: USING EXCEPTION + EXPANDER
-
-def test_silly():
-    a = 1
-    john = 'stud'
-    a += a
-    out = john*a
-    eq_( out, 'studstud' )
-    assert 0,'woo'
-
-
-class MyExpander(nose_Expander):
-    def __call__(self, ttype, tok, start, end, line):
-        logging.info('exp: %s', [ttype, tok, start, end, line])
-        oldsrc = self.expanded_source
-        nose_Expander.__call__(self, ttype, tok, start, end, line)
-        newsrc = self.expanded_source 
-        logging.info('+ %s',newsrc[len(oldsrc):])
-
-class MyExpander2(object):
-    class NoValue(object):
-        pass
-
-    def __init__(self, localvars, globalvars):
-        self.locals = localvars
-        self.globals = globalvars
-        self.expanded_source = ''
-
-    def value(self, varname):
-        return self.locals.get(varname, self.globals.get(varname, self.NoValue))
-
-    def __call__(self, ttype, tok, start, end, line):
-        if ttype != tokenize.NAME:
-            return
-        val = self.value(tok)
-        if 0:
-            logging.debug('\t%s', [ttype, tok, start, end, line, val, type(val).__name__])
-        if val is self.NoValue or type(val).__name__ in ['function',]:
-            return
-        logging.info(':%d:%d-%d %s %s', start[0], start[1], end[1], tok, val)
-
-inspector.Expander = MyExpander2 # XX monkeypatch
-
-
-def test_detail():
-    excinfo = None
-    try:
-        test_silly()
-    except AssertionError:
-        excinfo = sys.exc_info()
-    tb = excinfo[2]
-    print 'traceback', tb
-    
-    tbinfo = inspector.inspect_traceback(tb)
-    print tbinfo
-
-# :::::::::::::::::::::::::::::::::::::::::::::::::: USING TRACE
-import trace
-
-def test_tracelocals():
-    a = 1
-    longline = \
-        'testme'
-    john = 'stud'
-    def inner(arg=None):
-        xi = 2
-        return xi + 1
-    testfunc = inner(a)
-    a += a
-    out = john*a
 
 class TraceLocals(trace.Trace):
     def __init__(self):
@@ -109,9 +38,10 @@ class TraceLocals(trace.Trace):
 
 def test_vartrace():
     t = TraceLocals()
-    t.runfunc(test_tracelocals)
+    _mod = __import__('ex_snoop')
+    t.runfunc(_mod.test_tracelocals)
     del t.outvars['inner']   # a function
-    eq_( t.outvars, {'a': 2, 'john': 'stud', 'testfunc': 3, 'out': 'studstud'} )
+    eq_( t.outvars, {'a': 2, 'longline': 'testme', 'john': 'stud', 'testfunc': 3, 'out': 'studstud'} )
     print
     print '-'.join(t.lines[t.linerange[0]:t.linerange[1]])
 
