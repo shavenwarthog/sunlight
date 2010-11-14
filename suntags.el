8dcb81ccdf876094e8bc9692ccc5f1f83b27898c
John L Mitchell <johnlmitchell@gmail.com>
2010-11-14 12:59:13 -0800

    towards multiple hashtable

--- suntags.el
+++ suntags.el
@@ -1,56 +1,56 @@
 ;; suntags.el -- tags for multiple dimensions per item
 
-;; 1: def system(cmd):
-;; 2:   os.system(cmd)
-;; 3:
-;; 4: def testme():
-;; 5:   system("hostname")
+;; 3: def system(cmd):
+;; 4:   os.system(cmd)
+;; 5:
+;; 6: def testme():
+;; 7:   system("hostname")
 
-;; 'system' defined line 1, called on line 2 and 5
+;; 'system' defined line 3, called on line 7
 
 (defvar suntag-define-table (make-hash-table :test 'equal)
   "Hash of symbol definitions.")
   
-(defun suntag-parse (line)
+(defun suntag-parse (hash line)
   (when (string-match "\\([0-9]+\\):.+ \\(.+\\)" line)
     (let* ((lineno (match-string 1 line))
 	   (symname (match-string 2 line))
-	   (oldlist (gethash symname suntag-define-hash)))
-      (puthash symname (cons lineno oldlist) suntag-define-hash))))
+	   (oldlist (gethash symname hash)))
+      (puthash symname (cons lineno oldlist) hash))))
 
 (defun suntag-list ()
   (with-output-to-temp-buffer "*Suntags List*"
     (maphash #'(lambda (key value) (princ (format "%s\t%s\n" key value)))
-	     suntag-define-hash)))
+	     suntag-define-table)))
 
 
-(defun jmc-retest ()
+(defun jmc-test ()
   (clrhash suntag-define-table)
   (suntag-parsefile "suntags.el")
   (suntag-list))
 
 (defun suntag-parsefile (path)
-  (setq suntag-define-hash (make-hash-table :test 'equal))
-  (mapc 'suntag-parse 
+  (setq suntag-define-table (make-hash-table :test 'equal))
+  (mapc (apply-partially 'suntag-parse suntag-define-table)
 	(process-lines 
 	 "egrep" "-n" "--only-matching" "def\s+([[:alnum:]]+)" path)))
 
 (defun suntag-find-tag (tagname)
   (interactive)
-  (let ((tagval (gethash "system" suntag-define-hash)))
+  (let ((tagval (gethash "system" suntag-define-table)))
     (if tagval
 	(goto-line (string-to-int (car tagval)))
       (error "No tags containing %s" tagname))))
 ;; (suntag-find-tag "system")
       
-;; (gethash "system" suntag-define-hash)
+;; (gethash "system" suntag-define-table)
 
 ;;   (interactive (find-tag-interactive "Find tag: "))
   
   
 ;; (defun jmc-retest () (suntag-parsefile "suntags.el"))
 ;; (defun jmc-retest () 
-;;   (let ((h (make-hash-table :test 'equal)))
+;;   (let ((h (make-table-table :test 'equal)))
 ;;     (puthash 'x (list "gin") h)
 ;;     (puthash 'x (append (gethash 'x h) (list "beer")) h)
 ;;     (message "woo: %s" (gethash 'x h))))
