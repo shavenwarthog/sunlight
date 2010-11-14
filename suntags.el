b248c8d9efca7318c58b7900ce64d3c4d49c545a
John L Mitchell <johnlmitchell@gmail.com>
2010-11-14 14:28:40 -0800

    supports X called by Y, multiple values per symbol

--- suntags.el
+++ suntags.el
@@ -10,30 +10,54 @@
 
 (defvar suntag-define-table (make-hash-table :test 'equal)
   "Hash of symbol definitions.")
+(defvar suntag-calledby-table (make-hash-table :test 'equal)
+  "Hash of X calls Y, keyed on Y.")
   
-(defun suntag-parse (hash line)
+(defun suntag-list1 (table)
+  (maphash #'(lambda (key value) (princ (format "%s\t%s\n" key value))) table))
+
+(defun suntag-list ()
+  (with-output-to-temp-buffer "*Suntags List*"
+    (princ "Definitions:\n")
+    (suntag-list1 suntag-define-table)
+    (princ "\nCalled by:\n")
+    (suntag-list1 suntag-calledby-table)))
+
+(defun jmc-test ()
+  (clrhash suntag-define-table)
+  (suntag-parse "suntags.el")
+  (suntag-list))
+
+(defun suntag-p-defs (table line)
   (when (string-match "\\([0-9]+\\):.+ \\(.+\\)" line)
     (let* ((lineno (match-string 1 line))
 	   (symname (match-string 2 line))
 	   (oldlist (gethash symname hash)))
       (puthash symname (cons lineno oldlist) hash))))
 
-(defun suntag-list ()
-  (with-output-to-temp-buffer "*Suntags List*"
-    (maphash #'(lambda (key value) (princ (format "%s\t%s\n" key value)))
-	     suntag-define-table)))
-
-
-(defun jmc-test ()
-  (clrhash suntag-define-table)
-  (suntag-parsefile "suntags.el")
-  (suntag-list))
-
-(defun suntag-parsefile (path)
+(defun suntag-parse-defs (path)
   (setq suntag-define-table (make-hash-table :test 'equal))
-  (mapc (apply-partially 'suntag-parse suntag-define-table)
+  (mapc (apply-partially 'suntag-p-defs suntag-define-table)
 	(process-lines 
 	 "egrep" "-n" "--only-matching" "def\s+([[:alnum:]]+)" path)))
+
+(defun suntag-p-calledby (table line)
+  (let ((words (split-string line)))
+    (message "woo")
+    (when (= 4 (length words))
+      (let* ((lineno (cadr words))
+	     (symname (nth 2 words))
+	     (oldlist (gethash symname table)))
+	(puthash symname (cons (cons lineno (nth 3 words)) oldlist) table)))))
+
+(defun suntag-parse-calledby (path)
+  (setq suntag-calledby-table (make-hash-table :test 'equal))
+  (mapc (apply-partially 'suntag-p-calledby suntag-calledby-table)
+	(process-lines "python2.6" "./calledby.py" path)))
+
+(defun suntag-parse (path)
+  (suntag-parse-calledby path)
+  (suntag-parse-defs path))
 
 (defun suntag-find-tag (tagname)
   (interactive)
@@ -41,6 +65,7 @@
     (if tagval
 	(goto-line (string-to-int (car tagval)))
       (error "No tags containing %s" tagname))))
+
 ;; (suntag-find-tag "system")
       
 ;; (gethash "system" suntag-define-table)
