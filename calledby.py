4211a1f61c0d68293f1d21d9995ff82cb5ace3cf
John L Mitchell <johnlmitchell@gmail.com>
2010-11-14 16:55:00 -0800

    wip: confused

--- calledby.py
+++ calledby.py
@@ -1,35 +1,61 @@
 #!/usr/bin/env python2.6
 
-import sys, token, tokenize
+import os, sys, token, tokenize
 
-def mytokens(path):
-    for ttype,tstring,(srow,_),_,tline in tokenize.generate_tokens(open(path).readline):
-        yield (ttype, tstring, srow)
 
-def find_calls(tokiter):
+def find_callsdefs(path):
     pin = []
-    for (ttype,tstring,srow) in tokiter:
-        pin.append( (ttype,tstring,srow) )
-        if (ttype==token.OP and tstring=='('):
-            yield pin[-3], pin[-2]
+    for info in tokenize.generate_tokens(open(path).readline):
+        ttype,tstring,_st,_end,_line = info
+        if (ttype==token.OP and tstring=='('
+            and pin and pin[-1][0]==token.NAME):
+            yield pin[-2:]
+            pin = []
+        else:
+            pin.append(info)
+
+def epath(path):
+    return '{path},0'.format(**locals())
+
+def etag(tagdef,name,lineno,offset):
+    return '{tagdef}\x7f{name}\x01>{lineno},{offset}'.format(**locals())
 
 def procpath(path):
+    if os.path.splitext(path)[-1] != '.py':
+        return
+
     defs = []
     calls = []
-    for (_,pstr,row),(_,name,_) in find_calls(mytokens('example.py')):
-        if pstr=='def':
-            defs.append( (row,name) )
+    for info in find_callsdefs(path):
+        if info[-1][1] in ('def','class'):
+            defs.append( info )
         else:
-            calls.append( (row,name,defs[-1][-1]) )
-    for row,name in defs:
-        print name,path,row
-    for row,name,calledby in calls:
-        print name,path,row,calledby
+            calledby = defs[-1][-1] if defs else '' # global
+            calls.append( list(info) + [calledby] )
+            print calls[-1]; sys.exit(1)
+    return defs,calls
+
+    # if 0:
+    #     for row,name in defs:
+    #         print name,path,row
+    #     for row,name,calledby in calls:
+    #         print name,path,row,calledby
+    # else:
+def mketags(outf, path, infos):
+    print >>outf, epath(path)
+    for info in infos:
+        _ttype,tstring,st,_end,line = info[:4]
+        print >>outf, etag(tagdef=tstring)
+# name,path,row
+#         for row,name,calledby in calls:
+#             print name,path,row,calledby
 
 
 if __name__=='__main__':
     for path in sys.argv[1:]:
-        procpath(path)
+        defs,calledby = procpath(path)
+        mketags( sys.stdout, path, calledby )
+
 
 
 
