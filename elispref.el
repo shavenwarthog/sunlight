df39371ee4af5f504c0552e692db734ac5befa5e
John L Mitchell <johnlmitchell@gmail.com>
2010-12-15 15:16:54 -0800

    works

--- elispref.el
+++ elispref.el
@@ -21,28 +21,35 @@
 ;; XXX: gauche but works
 
 (defun elispref-render (funcname html)
-  (insert lispref-html)
+  (html-mode)
+  (princ lispref-html)
   (elispref-replace "&mdash;" "-")
   (sgml-tags-invisible t))
 
 (defun elispref (funcname)
   (interactive "aFunction: ")
   (save-excursion
-    (set-buffer (find-file-noselect "~/Documents/elisp.html"))
+    (set-buffer (find-file-noselect "~/Documents/elisp.html" nil t))
+    (setq buffer-read-only t)		;;XX?
     (goto-char (point-min))
     (re-search-forward (format "<b>%s</b>" funcname))
     (let* ((start (re-search-backward "<div"))
 	   (end (re-search-forward "</div>"))
 	   (lispref-html (buffer-substring-no-properties start end)))
-      ;; XXXXX: bah:
-      ;; (kill-buffer (get-buffer (format "*elisp: %s*" funcname)))
-      (save-excursion
-	(set-buffer (get-buffer-create (format "*elisp: %s*" funcname)))
-	;; (erase-buffer)
-	(html-mode)
-	;; XX:
-	(switch-to-buffer-other-window (current-buffer))
-	(elispref-render funcname lispref-html)))))
+    (save-excursion
+      (with-help-window (help-buffer)
+	(set-buffer (help-buffer))
+      	(elispref-render funcname lispref-html))))))
+
+      ;; ;; XXXXX: bah:
+      ;; ;; (kill-buffer (get-buffer (format "*elisp: %s*" funcname)))
+      ;; (save-excursion
+      ;; 	(set-buffer (get-buffer-create (format "*elisp: %s*" funcname)))
+      ;; 	;; (erase-buffer)
+      ;; 	(html-mode)
+      ;; 	;; XX:
+      ;; 	(switch-to-buffer-other-window (current-buffer))
+      ;; 	(elispref-render funcname lispref-html)))))
 
 
 (defun jmc-test () (elispref "mapcar"))
