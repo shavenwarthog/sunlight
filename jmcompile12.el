4817d5da48408e7bf96f78f20a26ca23d648664f
John Mitchell <johnlmitchell@gmail.com>
2012-03-07 09:58:36 -0800

    if in compiliation buffer, do (recompile)

--- jmcompile12.el
+++ jmcompile12.el
@@ -1,6 +1,11 @@
 ;; John's Compile Helpers
 
 
+;; XX buffer-local:
+(defvar jmc-makefile-name "jm.mk" "Makefile for JMC project")
+(set-variable 'jmc-makefile-name "dev.mk")
+
+
 (defun jmc-get-compilation-directory (makefile)
   (let ((mkpath (locate-file makefile '("." ".."))))
     (when mkpath
@@ -17,7 +22,7 @@ With prefix arg, always test module.
 "
   (interactive)
   (save-some-buffers t)
-  (let ((compilation-directory (jmc-get-compilation-directory "dev.mk"))
+  (let ((compilation-directory (jmc-get-compilation-directory jmc-makefile-name))
 	(compile-command 
 	 (format
 	  "make -f dev.mk TEST_FUNCTION=%s mytests"
@@ -28,20 +33,39 @@ With prefix arg, always test module.
     (recompile)))
 
 
-(defun jmc-python-test-something ()
+
+(defun jmc-executablep (path)
+  (and (not (null path))
+       (> (logand (file-modes path) 73) 0)))
+
+
+(defun jmc-runbuffer ()
+  (interactive)
+  (let ((compile-command 
+	 (format "%s" (buffer-file-name))))
+    (recompile)))
+
+
+(defun jmc-retest ()
   "Depending on context, test current testfunction, current
 testmodule, or current source module.
 
+CONTEXT                 TEST
 test_beer.py:testfunc	just testfunc
 test_beer.py		whole test_beer.py module
 beer.py			tests/test_beer.py
+beer.py is +x		run ./beer.py
+*compilation*		recompile
 "
   (interactive)
-  (if (jmc-testmodulep)
-      (jmc-make-mytests (which-function))
-    (jmc-make-mytests "")))
-
-
+  (cond ((jmc-testmodulep)
+	 (jmc-make-mytests (which-function)))
+	((jmc-executablep (buffer-file-name))
+	 (jmc-runbuffer))
+	((string= mode-name "Compilation")
+	 (recompile))
+	(t
+	 (jmc-make-mytests ""))))
 
 
 (defun jmc-insert-assertvalue ()
@@ -53,6 +77,12 @@ beer.py			tests/test_beer.py
     (if match
 	(insert match)
       (message "AsserionError not found in compilation buffer."))))
+
+
+;; (add-hook 'find-file-hooks
+;;  (lambda ()
+;;    (when buffer-read-only
+;;      (set-background-color "yellow"))))
 
 
 (global-set-key (kbd "<f12>") 'jmc-insert-assertvalue)
